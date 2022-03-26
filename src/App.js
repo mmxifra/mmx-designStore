a82b65025e059c845d6a0cade2f29de2f89edda8
mmxifra <mmxifra@gmail.com>
2022-03-26 20:52:19 -0300

    agregue icono carrito y mensaje bienvenida

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar';
+import ItemListContainer from './components/ItemListContainer';
 
 function App() {
   return (
-    <>
-    <Navbar />
-    </>
+    <div className='container-fluid App'>
+      <Navbar />
+      <ItemListContainer greeting='Bienvenido a MMX Diseño' />
+    </div>
   );
 }
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,31 +1,26 @@
 import logo from '../logo.svg';
-import cart from '../cart.svg'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import CartWidget from './CartWidget';
 
 const Navbar =() =>{
     return (
-        <div className='container-fluid'>
+        <>
             <header className='row header navbar-expand-lg navbar-light'>
                 <div className='col-lg-3 col-md-12 col-xs-12 headerLogo'>
+                <a href='#'>
                     <img src={logo} className='brandLogo' alt='brand Logo' />
+                </a>
                 </div>
                 <div className='col-lg-6 col-md-12 col-xs-12 headerNav'>
-                    <h1 className='brandName'> MMX Diseño textil </h1>
+                    <h1 className='brandName'> MmX Diseño </h1>
                     <nav className='nav'>
-                        <a className='nav-link active navItem' aria-current='page' href='#'>Home</a>
                         <a className='nav-link navItem' href='#'>Cilindro</a>
                         <a className='nav-link navItem' href='#'>Schablon</a>
                         <a className='nav-link navItem' href='#'>Sublimado/Digital</a>
                     </nav>
                 </div>
-                <div className='col-lg-3 col-md-12 col-xs-12 headerCart'>
-                    <button type='button' class='btn position-relative'>
-                        <img src={cart} className='cartIcon' alt='brand Logo' />
-                        <span class='position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger'>8</span>
-                    </button>
-                </div>
+                <CartWidget />
             </header>
-        </div>
+        </>
     );
 };
 
