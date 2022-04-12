df7715409d04a71be772e96f5e873c99ca10f039
mmxifra <mmxifra@gmail.com>
2022-04-12 18:26:55 -0300

    correccion className CartWidget

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -3,9 +3,9 @@ import cart from '../cart.svg';
 const CartWidget = () => {
     return (
         <div className='col-lg-3 col-md-3 col-xs-12 headerCart'>
-            <button type='button' class='btn position-relative'>
+            <button type='button' className='btn position-relative'>
                 <img src={cart} className='cartIcon' alt='brand Logo' />
-                <span class='position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger'>8</span>
+                <span className='position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger'>8</span>
             </button>
         </div>
     );
